10ced5bc0b8d28907a8ddab527c4e32746fed51e
VŨ VĂN THÁI <64297334+AZZ-vopp@users.noreply.github.com>
2024-05-22 08:58:34 +0700

    Update fixarchive.sh

--- fixarchive.sh
+++ fixarchive.sh
@@ -2,14 +2,23 @@ sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
 
 sudo tee /etc/apt/sources.list > /dev/null <<EOL
 deb http://archive.ubuntu.com/ubuntu focal-updates main restricted
+
 deb http://archive.ubuntu.com/ubuntu focal universe
+
 deb http://archive.ubuntu.com/ubuntu focal-updates universe
+
 deb http://archive.ubuntu.com/ubuntu focal multiverse
+
 deb http://archive.ubuntu.com/ubuntu focal-updates multiverse
+
 deb http://archive.ubuntu.com/ubuntu focal-backports main restricted universe multiverse
+
 deb http://archive.ubuntu.com/ubuntu focal-security main restricted
+
 deb http://archive.ubuntu.com/ubuntu focal-security universe
+
 deb http://archive.ubuntu.com/ubuntu focal-security multiverse
+
 EOL
 sudo apt update
 echo "Hoàn tất thay thế"
